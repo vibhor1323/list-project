6cd28dd2fbefd607b295493c716b8813ac630f8b
vibhor1323 <vibhor2313@mail.com>
2021-10-06 01:52:08 +0530

    planning components

---
+++ src/app/recepies/recepie-list/recepie-item/recepie-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecepieItemComponent } from './recepie-item.component';
+
+describe('RecepieItemComponent', () => {
+  let component: RecepieItemComponent;
+  let fixture: ComponentFixture<RecepieItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecepieItemComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecepieItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
