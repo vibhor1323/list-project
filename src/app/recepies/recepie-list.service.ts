8ab6f10e9b7266b0449e740746144550b14feb02
vibhor1323 <vibhor2313@mail.com>
2021-10-12 00:39:10 +0530

    updated ingredients can be added to shopping list from recipe

--- src/app/recepies/recepie-list.service.ts
+++ src/app/recepies/recepie-list.service.ts
@@ -1,17 +1,40 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingrediants } from "../shared/ingrediants.model";
+import { ShoppingService } from "../shopping-list/shopping-list.service";
 import { Recepie } from "./recepie-model";
 
+@Injectable()
 export class RecepieService{
     recipeSelected= new EventEmitter<Recepie>();
 
     private recipes: Recepie[]=[
-        new Recepie('A Test Recepie' , 'this is simply a text','https://www.vegrecipesofindia.com/wp-content/uploads/2021/05/rajma-recipe-1-580x580.jpg' ),
-      new Recepie('Another Test Recepie' , 'this is simply a text','https://www.vegrecipesofindia.com/wp-content/uploads/2021/05/rajma-recipe-1-580x580.jpg' )
+        new Recepie(
+            'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingrediants('Meat', 1),
+        new Ingrediants('French Fries', 20)
+      ]),
+      new Recepie('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingrediants('Buns', 2),
+        new Ingrediants('Meat', 1)
+      ])
     ];
+    constructor(private slService:ShoppingService){
+    }
     
     getRecipes(){
         return this.recipes.slice();
     
+    }
+
+    addIngrediantsToShoppingList(ingrediants:Ingrediants[]){
+
+        this.slService.addIngredientss(ingrediants);
     }
 }
 
