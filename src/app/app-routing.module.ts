07f093cfb972174fa6f46eaeb6d0dd6f20b4fc68
vibhor1323 <vibhor2313@mail.com>
2021-10-14 14:02:12 +0530

    added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
+import { AuthGuard } from "./auth/auth.guard";
 import { RecepieDetailComponent } from "./recepies/recepie-detail/recepie-detail.component";
 import { RecepieEditComponent } from "./recepies/recepie-edit/recepie-edit.component";
 import { RecipesResolverService } from "./recepies/recepie-resolver.service";
@@ -9,8 +10,8 @@ import { RecepiesComponent } from "./recepies/recepies.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes :Routes=[
-    {path:'',redirectTo:'/recipes',pathMatch:'full'},
-    {path:'recipes', component:RecepiesComponent, children:[
+    {path:'', redirectTo:'/recipes',pathMatch:'full'},
+    {path:'recipes', component:RecepiesComponent,canActivate:[AuthGuard], children:[
         {path:'new',component:RecepieEditComponent},
         {path:':id',component:RecepieDetailComponent,resolve:[RecipesResolverService]},
         {path:':id/edit',component:RecepieEditComponent}]},
