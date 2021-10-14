2d3cde0b4d3513772ea59af7341becd3fee4f0b5
vibhor1323 <vibhor2313@mail.com>
2021-10-14 13:07:01 +0530

    updated Autologinand logout feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { AuthComponent } from './auth/auth.component';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(
+    private authService:AuthService
+  ){}
+  ngOnInit(){
+    this.authService.autoLogin();
+  }
 
 
 }
