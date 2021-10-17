452305c9100538bf7567982f18e9910d4777e140
vibhor1323 <vibhor2313@mail.com>
2021-10-17 12:21:18 +0530

    alert dynamic componet added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { RecepieService } from './recepies/recepie-list.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-intercepter';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,9 @@ import { AuthInterceptorService } from './auth/auth-intercepter';
     RecepiesStartComponent,
     RecepieEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver, ViewChild } from "@angular/core";
 import { Form, NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 import { AuthResponseData, AuthService } from "./auth.service";
 
 @Component({
@@ -13,9 +15,11 @@ export class AuthComponent{
     isLoginMode= true;
     isLoading=false;
     error : string = "";
+    @ViewChild(PlaceholderDirective,{static:false}) alertHost!: PlaceholderDirective;
 
 
-    constructor(private authService: AuthService,private router:Router){}
+    constructor(private authService: AuthService,private router:Router,
+        private componentFactoryResolver: ComponentFactoryResolver){}
 
     onSwitchMode(){
         this.isLoginMode= !this.isLoginMode;
@@ -44,11 +48,24 @@ export class AuthComponent{
             errorMessage =>{
                 console.log(errorMessage );
                 this.error=errorMessage;
+                this.showErrorAlert(errorMessage)
                 this.isLoading=false;
             });
         
 
        
         form.reset();
+    }
+    onHandleError(){
+        this.error=null!;
+    }
+
+    private showErrorAlert(message:string){
+        // const alertcmp = new AlertComponent();
+        const alertCmpFactory=this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        const hostViewContainerRef = this.alertHost.viewContainerRef;
+        hostViewContainerRef.clear(); 
+        
+        hostViewContainerRef.createComponent(alertCmpFactory); 
     }
 }
